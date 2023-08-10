5f3e9fd929090d7fa2fce036e7c4338ebc963213
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2023-08-10 11:59:35 +0530

    themeScript bug fixing

--- src/app/themeScript.js
+++ src/app/themeScript.js
@@ -1,17 +1,17 @@
-(function () {
-  function setMode() {
-    const theme = window?.localStorage?.getItem("bf-theme") || 'dark'
-    console.log(window, document.body)
-    if (theme === "dark") {
-      window.isDark = true
-      document?.body?.setAttribute('data-mode', 'dark')
-    } else {
-      window.isDark = false
-      document?.body?.setAttribute('data-mode', 'light')
-    }
-  }
-  document.addEventListener("DOMContentLoaded", () => {
-    setMode()
-  });
-  setMode()
-})()
+// (function () {
+//   function setMode() {
+//     const theme = window?.localStorage?.getItem("bf-theme") || 'dark'
+//     console.log(window, document.body)
+//     if (theme === "dark") {
+//       window.isDark = true
+//       document?.body?.setAttribute('data-mode', 'dark')
+//     } else {
+//       window.isDark = false
+//       document?.body?.setAttribute('data-mode', 'light')
+//     }
+//   }
+//   document.addEventListener("DOMContentLoaded", () => {
+//     setMode()
+//   });
+//   setMode()
+// })()
