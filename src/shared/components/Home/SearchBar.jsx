c19173275e975b61d87197a6e6876d21575ac116
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2023-09-06 12:25:11 +0530

    empty search value api call bug related changes

--- src/shared/components/Home/SearchBar.jsx
+++ src/shared/components/Home/SearchBar.jsx
@@ -4,40 +4,42 @@ export default function SearchBar({ setQuestionsData }) {
   const [searchText, setsearchText] = useState('')
 
   useEffect(() => {
-    async function getSearchedQuestions () {
-      const response = await fetch(`https://bio-finder-app.vercel.app/api/home?size=15&search=${searchText}`)
-      const json = await response?.json()
-  
-      const obj = {}
-      const arr = []
-  
-      json?.data?.questions.forEach((question) => {
-        if (!obj[question?.categoryValue]) {
-          obj[question?.categoryValue] = {
-            q: question?.text,
-            id: question?.categoryValue
+    if (searchText) {  
+      async function getSearchedQuestions () {
+        const response = await fetch(`https://bio-finder-app.vercel.app/api/home?size=15&search=${searchText}`)
+        const json = await response?.json()
+    
+        const obj = {}
+        const arr = []
+    
+        json?.data?.questions.forEach((question) => {
+          if (!obj[question?.categoryValue]) {
+            obj[question?.categoryValue] = {
+              q: question?.text,
+              id: question?.categoryValue
+            }
           }
-        }
-      })
-  
-      json?.data?.ans.forEach((answer) => {
-        const objj = {
-          id: obj[answer._id]?.id,
-          question: obj[answer._id]?.q,
-          answers: answer.value
-        }
-  
-        arr.push(objj)
-      })
-  
-      setQuestionsData(arr)
-    }
+        })
+    
+        json?.data?.ans.forEach((answer) => {
+          const objj = {
+            id: obj[answer._id]?.id,
+            question: obj[answer._id]?.q,
+            answers: answer.value
+          }
+    
+          arr.push(objj)
+        })
+    
+        setQuestionsData(arr)
+      }
 
-    const debouncer = setTimeout(() => {
-      getSearchedQuestions()
-    }, 1000)
-    return () => {
-      clearTimeout(debouncer)
+      const debouncer = setTimeout(() => {
+        getSearchedQuestions()
+      }, 1000)
+      return () => {
+        clearTimeout(debouncer)
+      }
     }
   }, [searchText, setQuestionsData])
 
